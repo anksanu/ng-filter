20d9ee2f7285828019a95add58351542fc2b9c3f
Ankit Agrawal <agrawal.anksanu@gmail.com>
2015-01-28 08:21:12 +0530

    Fixed the indentation with a std 2 space

--- src/_ellipses.js
+++ src/_ellipses.js
@@ -4,11 +4,11 @@ what I Do : shrink a text with ellipses at the end
 @params : the net length of the string with ellipses
 **/
 
-angular.module('ngFilter').filter('ellipses',[function(){
-	return function(input, maxLength){
-		var defaultLength = input.length;
-		var length = maxLength ? maxLength : defaultLength;
-		var str = input.length<=length? input : input.substring(0,length-3) + '...';
-		return str;
-	}
+angular.module('ngFilter').filter('ellipses', [function() {
+  return function(input, maxLength) {
+    var defaultLength = input.length;
+    var length = maxLength ? maxLength : defaultLength;
+    var str = input.length <= length ? input : input.substring(0, length - 3) + '...';
+    return str;
+  }
 }]);
