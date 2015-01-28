f5b7aeef99fc1b093d773913375aab3efa8c2750
Ankit Agrawal <agrawal.anksanu@gmail.com>
2015-01-28 08:21:55 +0530

    Filter string Array to String which converts an array of string into a single , separated string

---
+++ src/_stringArrayToString.js
@@ -0,0 +1,21 @@
+/**
+Filter Name : stringArrayToString
+what I Do : convert and array of string into a single string with ',' sperated array entities
+@params : Number or elements that need to be added in the string
+**/
+
+angular.module('ngFilter').filter('stringArrayToString', [function() {
+  return function(input, maxLength) {
+    var resultString = '';
+    maxLength = !maxLength ? input.length : maxLength;
+    maxLength = input.length < maxLength ? input.length : maxLength;
+    if (maxLength == 1) {
+      return input[0];
+    }
+    for (var i = 0; i < (maxLength - 2); i++) {
+    	resultString += input[i] + ', ';
+    }
+    resultString += input[maxLength-2] + ' and ' + input[maxLength-1]
+    return resultString;
+  }
+}]);
