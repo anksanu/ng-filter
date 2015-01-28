bd25fa697f227280d64b57487e704fe83bf0a708
Ankit Agrawal <agrawal.anksanu@gmail.com>
2015-01-28 08:22:41 +0530

    moved from html based filter test case rendering to a log based check

--- test/app.js
+++ test/app.js
@@ -1,7 +1,37 @@
 angular.module('AnkFilters',['ngFilter']);
 
 angular.module('AnkFilters').controller('ankFilterCtrl',[
-	'$scope',
-	function($scope){
-		$scope.text = "this is an ellipses filter";
+	'$scope','$filter',
+	function($scope,$filter){
+		var text = "this is an ellipses filter";
+		var arrayOfString1 = ['alpha'];
+		var arrayOfString2 = ['alpha','beta','gaama'];
+		var arrayOfString3 = ['alpha','beta'];
+		var arrayOfString4 = ['alpha','beta','gaama','delta'];
+
+		function log(){
+			console.log(arguments[0]);
+		}
+
+		log('Filter Name stringArrayToString');
+		log('Test Arrays ->');
+		log(arrayOfString1);
+		log(arrayOfString2);
+		log(arrayOfString3);
+		log(arrayOfString4);
+		log('With Null Params ->');
+		log($filter('stringArrayToString')(arrayOfString1));
+		log($filter('stringArrayToString')(arrayOfString2));
+		log($filter('stringArrayToString')(arrayOfString3));
+		log($filter('stringArrayToString')(arrayOfString4));
+		log('With Param 3 ->');
+		log($filter('stringArrayToString')(arrayOfString1,3));
+		log($filter('stringArrayToString')(arrayOfString2,3));
+		log($filter('stringArrayToString')(arrayOfString3,3));
+		log($filter('stringArrayToString')(arrayOfString4,3));
+		log('With Param 8 ->');
+		log($filter('stringArrayToString')(arrayOfString1,8));
+		log($filter('stringArrayToString')(arrayOfString2,8));
+		log($filter('stringArrayToString')(arrayOfString3,8));
+		log($filter('stringArrayToString')(arrayOfString4,8));
 }]);
