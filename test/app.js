1ce8911fb2e05376de4b0423f696175efb23e4dd
Ankit Agrawal <agrawal.anksanu@gmail.com>
2015-01-29 08:36:24 +0530

    supporting test cases for urlEncodeDecode filter

--- test/app.js
+++ test/app.js
@@ -34,4 +34,17 @@ angular.module('AnkFilters').controller('ankFilterCtrl',[
 		log($filter('stringArrayToString')(arrayOfString2,8));
 		log($filter('stringArrayToString')(arrayOfString3,8));
 		log($filter('stringArrayToString')(arrayOfString4,8));
+
+
+		var uri = "http://test.com?text=abc bcd";
+		var uriE = "http%3A%2F%2Ftest.com%3Ftext%3Dabc%20bcd";
+		log('Filter Name urlEncodeDecode');
+		log('Test Url ->');
+		log(uri);
+		log(uriE);
+		log('URI encoding ->');
+		log($filter('urlEncodeDecode')(uri));
+		log('URI Decoding ->');
+		log($filter('urlEncodeDecode')(uriE));
+		log($filter('urlEncodeDecode')(uriE,'decode'));
 }]);
