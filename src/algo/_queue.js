52c58e903404a50c77b12a1579c0625eb398ff6d
Ankit Agrawal <agrawal.anksanu@gmail.com>
2015-01-31 17:31:08 +0530

    added init code for algo module queue DS initial structure

---
+++ src/algo/_queue.js
@@ -0,0 +1,21 @@
+function ListItem(value, next, prev) {
+  this._value = value || void 0;
+  this._next = next || void 0;
+  this._prev = prev || void 0;
+}
+
+ListItem.prototype.get = function() {
+  return this._value;
+}
+
+ListItem.prototype.getNext = function() {
+  return this._next;
+}
+
+ListItem.prototype.getPrev = function() {
+  return this._prev;
+}
+
+function Queue() {
+ 	var _queue = Object.create({});
+}
