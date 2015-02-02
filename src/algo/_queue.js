bead5368b5b7579397e7905d24697572d58ea606
Ankit Agrawal <agrawal.anksanu@gmail.com>
2015-02-02 21:30:19 +0530

    Added routine definition for the queue

--- src/algo/_queue.js
+++ src/algo/_queue.js
@@ -12,16 +12,55 @@ ListItem.prototype.getNext = function() {
   return this._next;
 }
 
+ListItem.prototype.setNext = function(next) {
+  this._next = next;
+  return this;
+}
+
 ListItem.prototype.getPrev = function() {
   return this._prev;
 }
 
-function Queue() {
-  var _queue = Object.create({});
+ListItem.prototype.setPrev = function(prev) {
+  this._prev = prev;
+  return this;
 }
 
-Queue.prototype.push = function() {
+function Queue() {
+  this._queue = Object.create({});
+  this._length = 0;
+  this._front = void 0;
+  this._rear = void 0;
+}
 
+Queue.prototype.uniqueHash = function() {
+  return Math.floor(new Date().getTime()*Math.random(5));
+}
+
+Queue.prototype.peek = function() {
+  if (this._front) {
+    return this._queue[this._front].get();
+  }
+  return null;
+}
+
+Queue.prototype.push = function(value) {
+  var id = this.uniqueHash();
+  if (!this._length) {
+    var item = new ListItem(value, void 0, void 0);
+    this._front = id;
+    this._rear = id;
+    this._queue[id] = item;
+    this._length += 1;
+    return true;
+  }
+
+  var item = new ListItem(value, void 0, this._rear);
+  this._queue[this._rear]._next = id;
+  this._rear = id;
+  this._queue[id] = item;
+  this._length += 1;
+  return true;
 }
 
 Queue.prototype.pop = function() {
@@ -37,5 +76,21 @@ Queue.prototype.size = function() {
 }
 
 Queue.prototype.toString = function() {
-
+  var tempId = this._front;
+  var arr = [];
+  for (var i = 0; i < this._length; i++) {
+  	if(!tempId)
+  		continue;
+    arr.push(this._queue[tempId].get());
+    tempId = this._queue[tempId].getNext();
+  }
+  return arr.join(',');
 }
+
+
+var q = new Queue();
+q.push(5);
+q.push(6);
+q.push(7);
+q.push(8);
+console.log(q.toString());
