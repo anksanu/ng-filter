730f60bfc2184bb4869429ea51ebd07bdadd1bc1
Ankit Agrawal <agrawal.anksanu@gmail.com>
2015-02-05 08:16:34 +0530

    basic structure of binary tree add Node functionality added

--- src/algo/_binaryTree.js
+++ src/algo/_binaryTree.js
@@ -1,18 +1,48 @@
-function BinaryTree () {
+function BinaryTree() {
   this._tree = Object.create({});
   this._root = void 0;
 }
 
 BinaryTree.prototype.getNewHash = function() {
-  return Math.floor(new Date.getTime() * Math.random(5));
+  return Math.floor(new Date().getTime() * Math.random(5));
 };
 
-BinaryTree.prototype.addNode = function(value){
+BinaryTree.prototype.addNode = function(value) {
+  var newNode = new Node(value, this._root);
+  var id = this.getNewHash();
+  if (!this._root) {
+    this._root = id;
+    this._tree[id] = newNode;
+    return;
+  }
 
+
+  function lookUp(nodeId, tree, value) {
+    var node = tree[nodeId];
+    var nodeValue = node.get();
+    var nodeLeft = node.children().left;
+    var nodeRight = node.children().right;
+    if (nodeValue >= value) {
+      return nodeLeft != undefined ? lookUp(nodeLeft,tree,value) : nodeId;
+    } else {
+      return nodeRight != undefined ? lookUp(nodeRight,tree,value) : nodeId;
+    }
+  }
+
+  var parentNodeId = lookUp(this._root, this._tree, value);
+  var parentNode = this._tree[parentNodeId];
+  var parentNodeValue = parentNode.get();
+  newNode.setParent(parentNodeId);
+  if (parentNodeValue >= value) {
+    parentNode.setLeftChild(id);
+  } else {
+    parentNode.setRightChild(id);
+  }
+  this._tree[id] = newNode;
 };
 
 BinaryTree.prototype.isEmpty = function() {
-  if(!this._root)
+  if (!this._root)
     return true;
   return false;
 };
@@ -21,6 +51,13 @@ BinaryTree.prototype.getRootNode = function() {
   return this._root;
 };
 
-BinaryTree.prototype.toString = function(){
-  
+BinaryTree.prototype.toString = function() {
+
 };
+
+var tree = new BinaryTree();
+tree.addNode(5);
+tree.addNode(4);
+tree.addNode(7);
+tree.addNode(6);
+console.table(tree._tree);
