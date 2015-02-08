5786537fa0f036ff5c42b9721642bcea47bc2eb4
Ankit Agrawal <agrawal.anksanu@gmail.com>
2015-02-08 21:28:20 +0530

    added comments in the tree node file

--- src/algo/_treeNode.js
+++ src/algo/_treeNode.js
@@ -1,3 +1,4 @@
+// Tree Node constructor
 function Node (value, parent, leftChild, rightChild) {
   this._value = value || void 0;
   this._parent = parent || void 0;
@@ -5,26 +6,44 @@ function Node (value, parent, leftChild, rightChild) {
   this._rightChild = rightChild || void 0;
 }
 
+
+
+// returns the value of the node
 Node.prototype.get = function() {
   return this._value;
 };
 
+
+
+// returns the parent of the node
 Node.prototype.parent = function() {
   return this._parent;
 };
 
+
+
+// set the parent of the node
 Node.prototype.setParent = function(parent) {
   this._parent = parent;
 };
 
+
+
+//set the left child of the node
 Node.prototype.setLeftChild = function(leftChild){
   this._leftChild = leftChild;
 };
 
+
+
+//set the right child of the node
 Node.prototype.setRightChild = function(rightChild){
   this._rightChild = rightChild;
 };
 
+
+
+// retuns the children of the node { left : '' , right : ''}
 Node.prototype.children = function() {
    return {
      left : this._leftChild,
@@ -32,6 +51,8 @@ Node.prototype.children = function() {
    };
 };
 
+
+// returns if the node is leaf or not
 Node.prototype.isLeaf = function() {
   if(!this._leftChild && !this._rightChild)
     return true;
